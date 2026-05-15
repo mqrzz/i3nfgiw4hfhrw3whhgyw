69d7cfc6705e3f9ba81875dc4eaab7da12d7aaf7
mqrzz <hzzz42375@gmail.com>
2026-05-15 16:26:13 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -18,4 +18,4 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 
 // ВАШ email — только для admin.html
-export const ADMIN_EMAIL = "ВСТАВЬ_СВОЮ_ПОЧТУ@gmail.com";
+export const ADMIN_EMAIL = "wbtipoofficialcom@gmail.com";
